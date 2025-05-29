f30e56ba4ba8abe105ad3700c319b6960653beca
estinto1224 <estinto1224@gmail.com>
2025-05-29 16:41:44 +0900

    Update routing configuration for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import ShopPage from './webshop';
 import ShopPage2 from './webshop2';
@@ -11,9 +11,9 @@ import AdminPage from './admin/AdminPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Webshop1">
       <Routes>
-        <Route path="/" element={<ShopPage />} />
+        <Route path="/" element={<Navigate to="/shop4" replace />} />
         <Route path="/shop2" element={<ShopPage2 />} />
         <Route path="/shop3" element={<ShopPage3 />} />
         <Route path="/shop4" element={<ShopPage4 />} />
