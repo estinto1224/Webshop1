9a3db070b0e9a7ebcacc767a0b6ac48c5da76d53
estinto1224 <estinto1224@gmail.com>
2025-05-29 16:54:56 +0900

    ttt
    tttt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import ShopPage2 from './webshop2';
 import ShopPage3 from './webshop3';
@@ -10,9 +10,9 @@ import AdminPage from './admin/AdminPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Webshop1">
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ShopPage4 />} />
+        <Route path="/*" element={<ShopPage4 />} />
         <Route path="/shop2" element={<ShopPage2 />} />
         <Route path="/shop3" element={<ShopPage3 />} />
         <Route path="/shop4" element={<ShopPage4 />} />
