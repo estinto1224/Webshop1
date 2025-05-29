e86a78b0c631c9b803a07cfef6dfe2279f9407aa
estinto1224 <estinto1224@gmail.com>
2025-05-29 16:50:35 +0900

    Fix routing and homepage configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
-import ShopPage from './webshop';
 import ShopPage2 from './webshop2';
 import ShopPage3 from './webshop3';
 import ShopPage4 from './webshop4';
@@ -11,14 +10,14 @@ import AdminPage from './admin/AdminPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/Webshop1">
       <Routes>
-        <Route path="/" element={<Navigate to="/shop4" replace />} />
+        <Route path="/" element={<ShopPage4 />} />
         <Route path="/shop2" element={<ShopPage2 />} />
         <Route path="/shop3" element={<ShopPage3 />} />
         <Route path="/shop4" element={<ShopPage4 />} />
         <Route path="/admin/*" element={<AdminPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 ); 
